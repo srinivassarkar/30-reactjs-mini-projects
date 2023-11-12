e729066f523409b7543c91d099fa28fa63bba2c4
Srinivas Sarkar <srinivassarkar07@gmail.com>
2023-11-12 15:42:45 +0530

    react-mini-projects

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl text-yellow-400 font-bold underline text-center">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
