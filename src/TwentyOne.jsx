3aefec68abbc3f2de082008b7073eefa0da42231
Srinivas Sarkar <srinivassarkar07@gmail.com>
2023-11-17 03:21:10 +0530

    react mini project

--- src/TwentyOne.jsx
+++ src/TwentyOne.jsx
@@ -1,6 +1,32 @@
+import { useReducer } from "react";
 
-export default function TwentyOne() {
+const initialState = 0;
+
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "RESET":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+const TwentyOne = () => {
+  const [state, dispatch] = useReducer(counterReducer, initialState);
+
   return (
-    <div>TwentyOne</div>
-  )
-}
+    <div>
+      <p>Counter App</p>
+      <button onClick={() => dispatch({ type: "INCREMENT" })}>+</button>
+      {state}
+      <button onClick={() => dispatch({ type: "DECREMENT" })}>-</button>
+      <p onClick={() => dispatch({ type: "RESET" })}>Reset</p>
+    </div>
+  );
+};
+
+export default TwentyOne
--- src/TwentyTwo.jsx
+++ src/TwentyTwo.jsx
@@ -1,32 +1,67 @@
 import { useReducer } from "react";
+import { useState } from "react";
 
-const initialState = 0;
+export default function TwentyOne() {
+  const [taskText, setTaskText] = useState("");
+  const taskReducer = (state, action) => {
+    switch (action.type) {
+      case "ADD_TASK":
+        return [
+          ...state,
+          { id: Date.now(), text: action.payload, completed: false },
+        ];
+      case "REMOVE":
+        return state.filter((task) => task.id !== action.payload);
+      case "TOGGLE_TYPE":
+        return state.map((task) =>
+          task.id === action.payload
+            ? { ...task, completed: !task.completed }
+            : task
+        );
+      default:
+        return state;
+    }
+  };
 
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    case "RESET":
-      return initialState;
-    default:
-      return state;
+  const [state, dispatch] = useReducer(taskReducer, []);
+
+  function addTask() {
+    dispatch({ type: "ADD_TASK", payload: taskText });
+    setTaskText("");
   }
-};
-
-const TwentyTwo = () => {
-  const [state, dispatch] = useReducer(counterReducer, initialState);
+  //console.log(state)
 
   return (
     <div>
-      <p>Counter App</p>
-      <button onClick={() => dispatch({ type: "INCREMENT" })}>+</button>
-      {state}
-      <button onClick={() => dispatch({ type: "DECREMENT" })}>-</button>
-      <p onClick={() => dispatch({ type: "RESET" })}>Reset</p>
+      <p>TO DO APP</p>
+      <input
+        type="text"
+        value={taskText}
+        placeholder="Add new task..."
+        onChange={(e) => setTaskText(e.target.value)}
+      />
+      <button onClick={addTask}>ADD TASK</button>
+      <ul>
+        {state.map((eachTask) => (
+          <li key={eachTask.id}>
+            <span
+              style={{
+                textDecoration: eachTask.completed ? "line-through" : "none",
+              }}
+              onClick={() =>
+                dispatch({ type: "TOGGLE_TYPE", payload: eachTask.id })
+              }
+            >
+              {eachTask.text}
+            </span>
+            <button
+              onClick={() => dispatch({ type: "REMOVE", payload: eachTask.id })}
+            >
+              REMOVE
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-};
-
-export default TwentyTwo;
+}
